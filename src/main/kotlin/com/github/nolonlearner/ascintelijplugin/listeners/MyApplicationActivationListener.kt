969896e0b22d00ae0fc49db307055c5f20525317
GitHub Action <action@github.com>
2024-10-18 01:25:04 +0000

    Template cleanup

--- src/main/kotlin/com/github/nolonlearner/ascintelijplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/nolonlearner/ascintelijplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.nolonlearner.ascintelijplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
